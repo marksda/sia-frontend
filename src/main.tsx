07c3a7c5c6a33b98c2838bc5386010e4c4776e37
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-05 23:10:44 +0700

    cso

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import App from './App.tsx';
 import './index.css';
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
     <FluentProvider theme={webLightTheme}>
       <App />
     </FluentProvider>
-  </React.StrictMode>,
- )
+);
