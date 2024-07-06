b097cc6a1a18bd6b3caa5e1f8f5990b147705536
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-06 17:28:36 +0700

    cso

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,21 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './app/redux-store.ts';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // import './index.css';
 
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <Home />,
+  //   errorElement: <ErrorPage />
+  // },
+  // {
+  //   path: "login",
+  //   element: <LoginPage />,
+  // },
+]);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -16,6 +29,7 @@ root.render(
     <PersistGate loading={null} persistor={persistor}>
       <FluentProvider theme={webLightTheme}>
         <App />
+        <RouterProvider router={router} />
       </FluentProvider>
     </PersistGate>    
   </Provider>    
