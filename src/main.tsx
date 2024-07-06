ebb00489ecaaf724905135f40fc7d991aba44df7
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-06 16:41:13 +0700

    cso

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 // import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import { Provider } from 'react-redux';
+import { persistor, store } from './app/redux-store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
-import './index.css';
+// import './index.css';
 
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(
-    <FluentProvider theme={webLightTheme}>
-      <App />
-    </FluentProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <FluentProvider theme={webLightTheme}>
+        <App />
+      </FluentProvider>
+    </PersistGate>    
+  </Provider>    
 );
