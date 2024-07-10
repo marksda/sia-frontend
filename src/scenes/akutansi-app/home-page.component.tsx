6f184334c530adfd013b6a7d3df2d09002ae2b25
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-10 21:18:47 +0700

    cso

--- src/scenes/akutansi-app/home-page.component.tsx
+++ src/scenes/akutansi-app/home-page.component.tsx
@@ -1,23 +1,15 @@
 import { Button } from "@fluentui/react-components";
 import type { ButtonProps } from "@fluentui/react-components";
 import { useAppSelector } from "../../features/state-engine/redux-hooks";
-import { Navigate, Outlet } from "react-router-dom";
-import SignInScreen from "./sign-in.component";
+import { Navigate } from "react-router-dom";
 
 
 
-function HomeScreen(props: ButtonProps) {    
+const HomeScreen = (props: ButtonProps) => {    
     const token = useAppSelector(state => state.persisted.token); 
 
-    return (
-        <>
-        {
-            token == null ?
-            <Navigate to="/login" replace={true} />
-            : <Button {...props}>Example</Button> 
-        }
-        </>
-         
-    );
+    return token == null ? <Navigate to="/login" replace={true} /> : <Button {...props}>Example</Button>;
 };
+
+export default HomeScreen;
 
