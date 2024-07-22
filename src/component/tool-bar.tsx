b2378ff29eb9e9eb5360004852622103b06e502c
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-22 10:25:04 +0700

    cso

---
+++ src/component/tool-bar.tsx
@@ -0,0 +1,57 @@
+import { makeStyles, Overflow, OverflowItem, Tab, TabList, tokens } from "@fluentui/react-components";
+import { FC, useState } from "react";
+
+//----- OverflowMenu -----//
+const useOverflowMenuStyle = makeStyles({
+    menu: {
+      backgroundColor: tokens.colorNeutralBackground1,
+    },
+    menuButton: {
+      alignSelf: "center",
+    },
+});
+
+export type ItemBar = {
+    id: string;
+    nama: string;
+    icon: React.ReactElement|null;
+};
+
+interface IToolBarProp {
+    data: ItemBar[]
+};
+
+
+const ToolBar: FC<IToolBarProp> = ({data}) => {
+    // const styles = useOverflowMenuStyle();
+    const [selectedTabId, setSelectedTabId] = useState<string>(data[0].id);
+  
+    const onTabSelect = (tabId: string) => {
+      setSelectedTabId(tabId);
+    };
+  
+    return (
+        <Overflow minimumVisible={2}>
+          <TabList
+            selectedValue={selectedTabId}
+            onTabSelect={(_, d) => onTabSelect(d.value as string)}
+          >
+            {data.map((item) => {
+              return (
+                <OverflowItem
+                  key={item.id}
+                  id={item.id}
+                  priority={item.id === selectedTabId ? 2 : 1}
+                >
+                  <Tab value={item.id}>
+                    {item.nama}
+                  </Tab>
+                </OverflowItem>
+              );
+            })}
+          </TabList>
+        </Overflow>
+    );
+};
+
+export default ToolBar;
--- src/layout/akutansi-app/portrait/rekening-portrait.tsx
+++ src/layout/akutansi-app/portrait/rekening-portrait.tsx
@@ -1,26 +1,49 @@
-import { makeStyles } from "@fluentui/react-components";
+import { makeStyles, tokens } from "@fluentui/react-components";
 import { FC } from "react";
+import ToolBar, { ItemBar } from "../../../component/tool-bar";
 
 
 const useStyles = makeStyles({
     root: {
         display: "grid",
-        gridTemplateRows: "auto 40px",
+        gridTemplateRows: "auto 58px",
         gridTemplateColumns: "auto",
+        height: "100%"
     },
     main: {
         // flex: 1,
         // flexGrow: 4,
         // background: "red",
-        
+        // height: "fit-content",
     },
     toolBar: {
-        // height: "40px",
-        // background: "yellow",
-        // alignContent: "end"
-        // alignItems: "end"
+        borderTop: `1px solid  ${tokens.colorNeutralBackground3Selected}`,        
+        // boxShadow: "rgba(0, 0, 0, 0.1) 0px -1px 0px 0px inset",
     }
 });
+
+const DataItemBars: ItemBar[] = [
+    {
+        id: "akun",
+        nama: "Akun",
+        icon: null
+    },
+    {
+        id: "jurnal",
+        nama: "Jurnal",
+        icon: null
+    },
+    {
+        id: "buku_pembantu",
+        nama: "Buku Pembantu",
+        icon: null
+    },
+    {
+        id: "buku_besar",
+        nama: "Buku Besar",
+        icon: null
+    },
+];
 
 const RekeningPortraitLayout: FC = () => {
     const styles = useStyles();
@@ -28,7 +51,9 @@ const RekeningPortraitLayout: FC = () => {
     return (
         <div className={styles.root}>
             <div className={styles.main}>main</div>
-            <div className={styles.toolBar}>bottom tool bar</div>
+            <div className={styles.toolBar}>
+                <ToolBar data={DataItemBars} />
+            </div>
         </div>
     );
 };
