1b3c6eda538d3cf455ab81ef3ee96d3abd721896
Syaiful Dwi Anwar <vlack123@gmail.com>
2024-07-26 10:46:25 +0700

    cso

--- src/layout/akutansi-app/portrait/pembukuan-portrait.tsx
+++ src/layout/akutansi-app/portrait/pembukuan-portrait.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { makeStyles, tokens } from "@fluentui/react-components";
+import { Dropdown, makeStyles, Option, tokens } from "@fluentui/react-components";
 import BottomNavBar from "../../../navigation/bottom-nav-bar";
 import {
     CalendarMonthRegular,
@@ -10,6 +10,7 @@ import {
 } from "@fluentui/react-icons";
 import { Outlet } from "react-router-dom";
 import { ItemBar } from "../../../features/entities/item-bar";
+import { useGetDaftarKelompokAkunQuery } from "../../../services/api-rtkquery-service";
 
 const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
 const Book = bundleIcon(BookFilled, BookRegular);
@@ -18,7 +19,7 @@ const Book = bundleIcon(BookFilled, BookRegular);
 const useStyles = makeStyles({
     root: {
         display: "grid",
-        gridTemplateRows: "auto 68px",
+        gridTemplateRows: "42px auto 68px",
         gridTemplateColumns: "auto",
     },
     main: {
@@ -27,9 +28,13 @@ const useStyles = makeStyles({
         // background: "red",
         // height: "fit-content",
     },
-    toolBar: {
+    topToolBar: {
+        padding: "4px",
+        borderBottom: `1px solid ${tokens.colorNeutralBackground3Selected}`,  
+    },
+    bottomToolBar: {
         paddingTop: "8px",
-        borderTop: `1px solid  ${tokens.colorNeutralBackground3Selected}`,  
+        borderTop: `1px solid ${tokens.colorNeutralBackground3Selected}`,  
     },
 });
 
@@ -80,13 +85,39 @@ const DataItemBars: ItemBar[] = [
 
 const PembukuanPortraitLayout: FC = () => {
     const styles = useStyles();
+    const { data: kelompokAkun } = useGetDaftarKelompokAkunQuery({
+        pageNumber: 1,
+        pageSize: 25,
+        filters: [],
+        sortOrders: [
+          {
+            fieldName: 'id',
+            value: 'ASC'
+          },
+        ],
+    });
 
     return (
         <div className={styles.root}>
+            <div className={styles.topToolBar}>
+            <Dropdown appearance="outline" size="medium">
+                <Option key="0">
+                    Semua
+                </Option>
+            {
+            kelompokAkun != undefined ? 
+                kelompokAkun.map((i) => (
+                <Option key={i.id}>
+                    {i.nama!}
+                </Option>
+                )) : null
+            }
+            </Dropdown>
+            </div>
             <div className={styles.main}>
                 <Outlet />
             </div>
-            <div className={styles.toolBar}>
+            <div className={styles.bottomToolBar}>
                 <BottomNavBar data={DataItemBars} />
             </div>
         </div>
